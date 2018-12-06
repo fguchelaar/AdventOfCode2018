195234d469157553bfbefa335dad9b77b14e60cf
Frank Guchelaar <fguchelaar@gmail.com>
2018-12-06 13:53:19 +0100

    introduced a `Point` class, since `CGPoint` has `CGFloats` as internal type and I was getting tired of doing `Int`-conversions all of the time

--- day-06-chronal-coordinates/main.swift
+++ day-06-chronal-coordinates/main.swift
@@ -2,71 +2,82 @@ import Foundation
 
 let input = try! String(contentsOfFile: "input.txt").trimmingCharacters(in: .whitespacesAndNewlines)
 
-extension CGPoint: Hashable {
+struct Point: Hashable {
+
+    var x: Int
+    var y: Int
+    
+    static var zero: Point {
+        return Point(x: 0, y: 0)
+    }
+    
     public var hashValue: Int {
         return x.hashValue << 32 ^ y.hashValue
     }
     
-    func distance(to other: CGPoint) -> Int {
-        return abs(Int(self.x) - Int(other.x)) + abs(Int(self.y) - Int(other.y))
+    func distance(to other: Point) -> Int {
+        return abs(self.x - other.x) + abs(self.y - other.y)
     }
 }
 
-var coordinateMap: [CGPoint: [CGPoint]] = input
+var coordinateMap: [Point: [Point]] = input
     .components(separatedBy: .newlines)
     .map {
         let xy = $0.components(separatedBy: ", ").map { Int($0)! }
-        let c = CGPoint(x: xy[0], y: xy[1])
+        let c = Point(x: xy[0], y: xy[1])
         return c
     }
-    .reduce(into: [CGPoint: [CGPoint]]()) {
-        $0[$1] = [CGPoint]()
+    .reduce(into: [Point: [Point]]()) {
+        $0[$1] = [Point]()
 }
 
 // find the grid's edges
 let minPoint = coordinateMap
     .keys
-    .reduce(CGPoint(x: Int.max, y: Int.max)) {
-        CGPoint(x: min($0.x, $1.x), y: min($0.y, $1.y))
+    .reduce(Point(x: Int.max, y: Int.max)) {
+        Point(x: min($0.x, $1.x), y: min($0.y, $1.y))
 }
 let maxPoint = coordinateMap
     .keys
-    .reduce(CGPoint.zero) {
-        CGPoint(x: max($0.x, $1.x), y: max($0.y, $1.y))
+    .reduce(Point.zero) {
+        Point(x: max($0.x, $1.x), y: max($0.y, $1.y))
 }
 
 // Keep a list of all coordinates that are on the edge of the grid
-var boundaryCoordinates = [CGPoint]()
-var contested = [CGPoint]()
+var boundaryCoordinates = [Point]()
+var contested = [Point]()
 
-var grid = [CGPoint: Int]()
+var grid = [Point: Int]()
 
-for x in Int(minPoint.x)...Int(maxPoint.x) {
-    for y in Int(minPoint.y)...Int(maxPoint.y) {
+for x in minPoint.x...maxPoint.x {
+    for y in minPoint.y...maxPoint.y {
         
-        let c = CGPoint(x: x, y: y)
+        let c = Point(x: x, y: y)
         
         // distance to all coordinates
-        let distances = coordinateMap.keys.reduce(into: [CGPoint: Int]()) {
+        let distances = coordinateMap.keys.reduce(into: [Point: Int]()) {
             $0[$1] = $1.distance(to: c)
         }
         
-        // Part 2: fill the grid with distances
+        // Part 2: fill the grid with the accumulated distances
         grid[c] = distances.values.reduce(0, +)
         
-        // minimal distance
+        // find the minimal distance for this c
         let minimal = distances.values.min()!
         
-        // closest coordinate(s)
+        // what is/are the closest coordinate(s)
         let closest = distances.filter { (arg) -> Bool in
             return arg.value == minimal
         }
         
         if closest.count == 1 {
-            if x == Int(minPoint.x) || y == Int(minPoint.y) || x == Int(maxPoint.x) || y == Int(maxPoint.y) {
+            // If this c belongs to an edge of the grid, the area is infinite, so we can discard this area's coordinate
+            // in the final result.
+            if x == minPoint.x || y == minPoint.y || x == maxPoint.x || y == maxPoint.y {
                 boundaryCoordinates.append(closest.first!.key)
             } else {
-                var list = coordinateMap[closest.first!.key] ?? [CGPoint]()
+                // Store this c in the array of Points of the coordinate to which this c belongs (the closest one)
+                var list = coordinateMap[closest.first!.key] ?? [Point]()
                 list.append(c)
                 coordinateMap[closest.first!.key] = list
             }
@@ -77,13 +88,14 @@ for x in Int(minPoint.x)...Int(maxPoint.x) {
     }
 }
 
+// filter out all coordinates that have an infinite area and sort by size
 let areas = coordinateMap
     .filter { !boundaryCoordinates.contains($0.key) }
     .map { ($0.key, $0.value.count) }
     .sorted { $0.1 > $1.1 }
 
 print("Part 1:")
-print(areas)
+print(areas.first!.1)
 
 print("Part 2:")
 print(grid.values.filter { $0 < 10000 }.count )
