8648ba93f2f3b3dc254c2e909e0a217111155804
Frank Guchelaar <fguchelaar@gmail.com>
2018-12-12 00:15:03 +0100

    pretty good performance-gain by calculating the value in `map` instead of `max`. The previous solution did a lot of calls to `valueOfSquare`, this cuts it more than in half

--- day-11-chronal-charge/main.swift
+++ day-11-chronal-charge/main.swift
@@ -14,9 +14,16 @@ func power(at pos: (x: Int, y: Int)) -> Int {
 
 func valueOfSquare(of size: Int, in grid: [[Int]], at pos: (x: Int, y: Int)) -> Int {
     return grid[(pos.y)..<(pos.y)+size]
-        .map { $0[(pos.x)..<(pos.x)+size] }
-        .flatMap { $0 }
+        .flatMap { $0[(pos.x)..<(pos.x)+size] }
         .reduce(0, +)
+}
+
+func maxValue(with size: Int) -> (x: Int, y: Int) {
+    let x = (0..<(grid.count - size + 1))
+        .flatMap { y in (0..<(grid.count - size + 1)).map { ($0, y)} }
+        .map { ($0.0, $0.1, valueOfSquare(of: size, in: grid, at: $0)) }
+        .max { $0.2 < $1.2 }!
+    return (x.0,x.1)
 }
 
 // initialize the grid
@@ -27,23 +34,13 @@ for y in 0..<grid.count {
     }
 }
 
-func maxValue(with size: Int) -> (x: Int, y: Int) {
-    let positions = (0..<grid.count-size)
-        .map { y in (0..<grid.count-size).map { ($0, y)} }
-        .flatMap { $0 }
-    
-    return positions.max {
-        valueOfSquare(of: size, in: grid, at: $0) < valueOfSquare(of: size, in: grid, at: $1)
-        }!
-}
-
-let max = maxValue(with: 3)
+let maxp1 = maxValue(with: 3)
 print("Part 1:")
-print("\(max.0 + 1),\(max.1 + 1)")
+print("\(maxp1.0 + 1),\(maxp1.1 + 1)")
 
 print("Part 2:")
 for s in (16...16) { // solution is at #16, doing them all takes long, found it by printing the subtotals
-    let maximum = maxValue(with: s)
-    let value = valueOfSquare(of: s, in: grid, at: maximum)
-    print ("\(value) = \(maximum.0 + 1),\(maximum.1 + 1),\(s)")
+    let maxp2 = maxValue(with: s)
+    let value = valueOfSquare(of: s, in: grid, at: maxp2)
+    print ("\(value) = \(maxp2.0 + 1),\(maxp2.1 + 1),\(s)")
 }
