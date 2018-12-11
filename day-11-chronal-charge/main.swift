71d02355838c5a6cfc4926bfc887925c3a5bc0d5
Frank Guchelaar <fguchelaar@gmail.com>
2018-12-11 19:43:42 +0100

    refactored for a little DRY and used Range + reduce instead of double for loop

--- day-11-chronal-charge/main.swift
+++ day-11-chronal-charge/main.swift
@@ -13,13 +13,10 @@ func power(at pos: (x: Int, y: Int)) -> Int {
 }
 
 func valueOfSquare(of size: Int, in grid: [[Int]], at pos: (x: Int, y: Int)) -> Int {
-    var sum = 0
-    for y in (pos.y)..<(pos.y)+size {
-        for x in (pos.x)..<(pos.x)+size {
-            sum += grid[y][x]
-        }
-    }
-    return sum
+    return grid[(pos.y)..<(pos.y)+size]
+        .map { $0[(pos.x)..<(pos.x)+size] }
+        .flatMap { $0 }
+        .reduce(0, +)
 }
 
 // initialize the grid
@@ -30,27 +27,23 @@ for y in 0..<grid.count {
     }
 }
 
-let allPositions = (0..<grid.count-3)
-    .map { y in (0..<grid.count-3).map { ($0, y)} }
-    .flatMap { $0 }
+func maxValue(with size: Int) -> (x: Int, y: Int) {
+    let positions = (0..<grid.count-size)
+        .map { y in (0..<grid.count-size).map { ($0, y)} }
+        .flatMap { $0 }
+    
+    return positions.max {
+        valueOfSquare(of: size, in: grid, at: $0) < valueOfSquare(of: size, in: grid, at: $1)
+        }!
+}
 
-let max = allPositions.max {
-    valueOfSquare(of: 3, in: grid, at: $0) < valueOfSquare(of: 3, in: grid, at: $1)
-}!
-
+let max = maxValue(with: 3)
 print("Part 1:")
 print("\(max.0 + 1),\(max.1 + 1)")
 
 print("Part 2:")
-for s in (16...16) { // solution is as #16, doing them all takes to long, found it by printing the subtotals
-
-    let positions = (0..<grid.count-s)
-        .map { y in (0..<grid.count-s).map { ($0, y)} }
-        .flatMap { $0 }
-
-    let max = positions.max {
-        valueOfSquare(of: s, in: grid, at: $0) < valueOfSquare(of: s, in: grid, at: $1)
-        }!
-    let value = valueOfSquare(of: s, in: grid, at: max)
-    print ("\(value) = \(max.0 + 1),\(max.1 + 1),\(s)")
+for s in (16...16) { // solution is at #16, doing them all takes long, found it by printing the subtotals
+    let maximum = maxValue(with: s)
+    let value = valueOfSquare(of: s, in: grid, at: maximum)
+    print ("\(value) = \(maximum.0 + 1),\(maximum.1 + 1),\(s)")
 }
