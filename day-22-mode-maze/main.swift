e388bb6ecd4c1f4355596b4e5f7ff01a170af0ab
Frank Guchelaar <fguchelaar@gmail.com>
2018-12-27 16:21:33 +0100

    improved perf by using a 'sorted array'

--- day-22-mode-maze/main.swift
+++ day-22-mode-maze/main.swift
@@ -6,7 +6,7 @@ struct Point: Hashable, Equatable {
         self.x = x
         self.y = y
     }
-    
+
     func n4() -> [Point] {
         return [
             Point(x-1, y),
@@ -21,7 +21,7 @@ let depth =  5355
 let target = Point(14, 796)
 
 var cache = [
-    Point(0,0): 0,
+    Point(0, 0): 0,
     target: 0
 ]
 
@@ -29,7 +29,7 @@ func geoIndex(for point: Point) -> Int {
     if let cached = cache[point] {
         return cached
     }
-    
+
     if point.y == 0 {
         cache[point] = point.x * 16807
     } else if point.x == 0 {
@@ -37,7 +37,7 @@ func geoIndex(for point: Point) -> Int {
     } else {
         cache[point] = erosionLevel(for: Point(point.x-1, point.y)) * erosionLevel(for: Point(point.x, point.y-1))
     }
-    
+
     return cache[point]!
 }
 
@@ -71,18 +71,18 @@ let p1 = (0...target.y).flatMap { y in
 print ("Part 1:")
 print (p1.reduce(0, +))
 
-func a🌟(start: Point, goal: Point) -> (path: [Point], time: Int) {
-    
+func a🌟(start: Point, goal: Point) -> Int {
+
     struct State: Hashable, Equatable {
         var point: Point
         var tool: Tool
-        
+
         func neighbors() -> [State] {
             return point.n4().map { neighbor in
-                
+
                 let currentType = regionType(for: point)
                 let neighborType = regionType(for: neighbor)
-                
+
                 var newTool = tool
                 if neighborType == .rocky {
                     if tool == .neither {
@@ -101,72 +101,61 @@ func a🌟(start: Point, goal: Point) -> (path: [Point], time: Int) {
             }
         }
     }
-    
+
     func cost(_ from: State, to: State) -> Int {
         return from.tool == to.tool ? 1 : 8
     }
-    
+
     func heuristic(_ from: Point, _ to: Point) -> Int {
         // Let's try manhattan
         return abs(from.x - to.x) + abs(from.y - to.y)
     }
-    
-    func reconstruct(cameFrom: [State: State], point: State) -> [Point] {
-        var path = [point.point]
-        var crnt = point
-        while let p = cameFrom[crnt] {
-            path.insert(p.point, at: 0)
-            crnt = p
-        }
-        return path
-    }
-    
+
     var closed = Set<State>()
     let startState = State(point: start, tool: .torch)
-    var open = [startState]
-    
-    var prev = [State: State]()
-    
+
+    var open = [(state: startState, cost: 0)]
+
     var gScore = [State: Int]()
     gScore[startState] = 0
-    
-    var fScore = [State: Int]()
-    fScore[startState] = heuristic(start, goal)
-    
-    var current: State
+
+    var current: (state: State, cost: Int)
     while !open.isEmpty {
-        
-        open.sort {fScore[$0]! < fScore[$1]! }
+
         current = open.removeFirst()
-        
-        if current.point == goal {
-            let cost = gScore[current]! + (current.tool == .torch ? 0 : 7)
-            return (reconstruct(cameFrom: prev, point: current), cost)
+
+        if current.state.point == goal {
+            return gScore[current.state]! + (current.state.tool == .torch ? 0 : 7)
         }
-        
-        closed.insert(current)
-        
-        current.neighbors()
+
+        closed.insert(current.state)
+
+        current.state.neighbors()
             .filter { !closed.contains($0) }
             .forEach { neighbor in
 
-                let g = gScore[current, default: Int.max] + cost(current, to: neighbor)
-                
-                if !open.contains(neighbor) {
-                    open.append(neighbor)
-                } else if g >= gScore[neighbor, default: Int.max] {
+                let g = gScore[current.state, default: Int.max] + cost(current.state, to: neighbor)
+
+                if g >= gScore[neighbor, default: Int.max] {
                     return
                 }
 
-                prev[neighbor] = current
                 gScore[neighbor] = g
-                fScore[neighbor] = heuristic(neighbor.point, goal) + g
+                let f = heuristic(neighbor.point, goal) + g
+
+                // find insert-point
+                var idx = 0
+                for _ in 0..<open.count {
+                    if open[idx].cost >= f {
+                        break
+                    }
+                    idx += 1
+                }
+                open.insert((neighbor, f), at: idx)
         }
     }
-    
-    return ([Point](), -1)
+    return -1
 }
 
 print ("Part 2:")
-print (a🌟(start: Point(0, 0), goal: target).time)
-
+print (a🌟(start: Point(0, 0), goal: target))
